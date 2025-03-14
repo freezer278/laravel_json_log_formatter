03365ae1bc847f2a0c7619e6d670c803cb1d33e5
freezer278 <17979033+freezer278@users.noreply.github.com>
2025-03-14 13:58:14 +0000

    Fix styling

--- src/JsonLogsFormatter.php
+++ src/JsonLogsFormatter.php
@@ -8,35 +8,35 @@ use Monolog\LogRecord;
 class JsonLogsFormatter extends NormalizerFormatter
 {
     public function __construct()
-{
-    parent::__construct('Y-m-d\TH:i:s.uP');
-}
+    {
+        parent::__construct('Y-m-d\TH:i:s.uP');
+    }
 
     public function format(LogRecord $record): string
-{
-    $recordData = parent::format($record);
+    {
+        $recordData = parent::format($record);
 
-    $message = [
-        'timestamp' => $recordData['datetime'],
-        'version' => config('json_log_formatter.schema_version', 1),
-        'application' => config('json_log_formatter.app_name', 'laravel'),
-    ];
-    if (isset($recordData['message'])) {
-        $message['message'] = $recordData['message'];
-    }
-    if (isset($recordData['channel'])) {
-        $message['environment'] = $recordData['channel'];
-    }
-    if (isset($recordData['level_name'])) {
-        $message['level'] = strtolower($recordData['level_name']);
-    }
-    if (count($recordData['context']) > 0) {
-        $message['context'] = $recordData['context'];
-    }
-    if (count($recordData['extra']) > 0) {
-        $message['extra'] = $recordData['extra'];
-    }
+        $message = [
+            'timestamp' => $recordData['datetime'],
+            'version' => config('json_log_formatter.schema_version', 1),
+            'application' => config('json_log_formatter.app_name', 'laravel'),
+        ];
+        if (isset($recordData['message'])) {
+            $message['message'] = $recordData['message'];
+        }
+        if (isset($recordData['channel'])) {
+            $message['environment'] = $recordData['channel'];
+        }
+        if (isset($recordData['level_name'])) {
+            $message['level'] = strtolower($recordData['level_name']);
+        }
+        if (count($recordData['context']) > 0) {
+            $message['context'] = $recordData['context'];
+        }
+        if (count($recordData['extra']) > 0) {
+            $message['extra'] = $recordData['extra'];
+        }
 
-    return $this->toJson($message) . "\n";
-}
+        return $this->toJson($message) . "\n";
+    }
 }
